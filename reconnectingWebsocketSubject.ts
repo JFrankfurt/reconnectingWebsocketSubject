f2bb6fa6c1b8fa82ba02568614b25c07aee17b2d
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2017-05-25 17:23:43 -0400

    Angular (2+) implementation of a reconnecting websocket subject

---
+++ reconnectingWebsocketSubject.ts
@@ -0,0 +1,98 @@
+
+import {Observable} from 'rxjs/Observable';
+import {Subject} from 'rxjs/Subject';
+import {Observer} from 'rxjs/Observer';
+import {WebSocketSubject, WebSocketSubjectConfig} from 'rxjs/observable/dom/WebSocketSubject';
+import {Injectable} from '@angular/core';
+import 'rxjs/add/observable/interval';
+import 'rxjs/add/operator/share';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/takeWhile';
+
+@Injectable()
+export class RxWebsocketSubject<T> extends Subject<T> {
+  private reconnectionObservable: Observable<number>;
+  private wsSubjectConfig: WebSocketSubjectConfig;
+  private socket: WebSocketSubject<any>;
+  private connectionObserver: Observer<boolean>;
+  public connectionStatus: Observable<boolean>;
+
+  defaultResultSelector = (e: MessageEvent) => JSON.parse(e.data);
+  defaultSerializer = (data: any): string => JSON.stringify(data);
+
+  constructor(private url: string,
+              private reconnectInterval = 12000,
+              private reconnectAttempts = 0,
+              private resultSelector?: (e: MessageEvent) => any,
+              private serializer?: (data: any) => string,) {
+    super();
+
+    this.connectionStatus = new Observable((observer) => {
+      this.connectionObserver = observer;
+    }).share().distinctUntilChanged();
+
+    if (!resultSelector) {
+      this.resultSelector = this.defaultResultSelector;
+    }
+    if (!this.serializer) {
+      this.serializer = this.defaultSerializer;
+    }
+
+    this.wsSubjectConfig = {
+      url: url,
+      closeObserver: {
+        next: () => {
+          this.socket = null;
+          this.connectionObserver.next(false);
+        }
+      },
+      openObserver: {
+        next: () => {
+          this.connectionObserver.next(true);
+        }
+      }
+    };
+    this.connect();
+    this.connectionStatus.subscribe((isConnected) => {
+      if (!this.reconnectionObservable && typeof(isConnected) === 'boolean' && !isConnected) {
+        this.reconnect();
+      }
+    });
+  }
+
+  connect(): void {
+    this.socket = new WebSocketSubject(this.wsSubjectConfig);
+    this.socket.subscribe(
+      (m) => {
+        this.next(m);
+      },
+      () => {
+        if (!this.socket) {
+          this.reconnect();
+        }
+      });
+  }
+
+  reconnect(): void {
+    this.reconnectionObservable = Observable.interval(this.reconnectInterval)
+      .takeWhile((v, index) => {
+        return index < this.reconnectAttempts && !this.socket;
+      });
+    this.reconnectionObservable.subscribe(
+      () => {
+        this.connect();
+      },
+      null,
+      () => {
+        this.reconnectionObservable = null;
+        if (!this.socket) {
+          this.complete();
+          this.connectionObserver.complete();
+        }
+      });
+  }
+
+  send(data: any): void {
+    this.socket.next(this.serializer(data));
+  }
+}
